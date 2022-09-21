7211066e0992da650357a7690b8f90d38c8c7824
HassanSalah25 <salemhassan175@gmail.com>
2022-09-21 02:19:33 +0200

    asdda

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,7 +26,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'password',//jkjbjbk
+        'password',//jkjbj
         'twitter_id',
         'oauth_type',
         'google_id'
